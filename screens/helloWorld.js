b7cf4e8e2512539eca4938a5832af3c8695bbe9e
kartini kartini <kartini.103@gmail.com>
2020-10-15 14:43:19 +0700

    This is my first commit

---
+++ screens/helloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HelloWorld = props => {
+    return (
+        <View>
+            <Text>Hi There</Text>
+        </View>
+    );
+};
+
+export default HelloWorld;
